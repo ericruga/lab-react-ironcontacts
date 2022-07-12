89b6e906ef3cb20f55c97a0532d654a246dd76d8
ericruga <ericruga@users.noreply.github.com>
2022-07-12 16:47:28 +0200

    Iteration 1 - not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import "./App.css";
+import contacts from './contacts';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+  <h2>IronContacts</h2>
+<table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+    {contacts.map((contact) => {
+      <tr>
+        <td>{contact.pictureUrl}</td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
+      </tr>
+  })}
+    </tbody>
+</table>
+</div>
+);
 }
 export default App;
+
